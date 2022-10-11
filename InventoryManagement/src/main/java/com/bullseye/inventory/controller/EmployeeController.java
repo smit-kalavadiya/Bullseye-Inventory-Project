f3e5c7841616e5ba5cad7820278f721e1ce20a56
smit kalavadiya <smitkalavadiya99@gmail.com>
2022-10-11 17:23:10 -0300

    Update EmployeeController.java

--- InventoryManagement/src/main/java/com/bullseye/inventory/controller/EmployeeController.java
+++ InventoryManagement/src/main/java/com/bullseye/inventory/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bullseye.inventory.entity.Employee;
@@ -20,7 +21,8 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService emp_ser;
 	
-	@GetMapping("/Employees")
+	@CrossOrigin(origins = "http://localhost:4200")
+	@RequestMapping("/Employee")
 	public List<Employee> getEmployee() {
 		return emp_ser.getAllEmployees();
 	}

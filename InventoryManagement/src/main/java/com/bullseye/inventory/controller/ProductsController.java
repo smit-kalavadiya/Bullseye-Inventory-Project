98b6e03c5123a342805a9fc92e8cc5e6182cb75e
smit kalavadiya <smitkalavadiya99@gmail.com>
2022-10-18 19:57:40 -0300

    main

--- InventoryManagement/src/main/java/com/bullseye/inventory/controller/ProductsController.java
+++ InventoryManagement/src/main/java/com/bullseye/inventory/controller/ProductsController.java
@@ -1,10 +1,25 @@
 package com.bullseye.inventory.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.bullseye.inventory.entity.Products;
+import com.bullseye.inventory.service.ProductsService;
 
 @RestController
 public class ProductsController {
 	
+	@Autowired
+	ProductsService service;
 	
+	@CrossOrigin(origins = "http://localhost:4200")
+	@GetMapping("/products")
+	public List<Products> getProducts(){
+		return service.getProducts();
+	}
 	
 }
--- InventoryManagement/src/main/java/com/bullseye/inventory/service/ProductsService.java
+++ InventoryManagement/src/main/java/com/bullseye/inventory/service/ProductsService.java
@@ -1,8 +1,11 @@
 package com.bullseye.inventory.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.bullseye.inventory.entity.Products;
 import com.bullseye.inventory.repository.ProductsRepository;
 
 @Service
@@ -10,5 +13,9 @@ public class ProductsService {
 	
 	@Autowired
 	ProductsRepository product_repo;
+
+	public List<Products> getProducts() {
+		return product_repo.findAll();
+	}
 
 }
